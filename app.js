f61480141c987fcdb300b0c7d75a781cd4df6b5c
jodywarren <jodytuuta@msn.com>
2026-03-12 09:53:38 +1100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { initState } from "./js/state.js";
 import { bindShellEvents } from "./js/render.js";
 import { bindIncidentInputs, loadIncidentIntoInputs } from "./js/incident.js";
 import { initResponders } from "./js/responders.js";
+import { bindOcrEvents } from "./js/ocr.js";
 
 document.addEventListener("DOMContentLoaded", initApp);
 
@@ -10,5 +11,6 @@ async function initApp() {
   bindShellEvents();
   bindIncidentInputs();
   loadIncidentIntoInputs();
+  bindOcrEvents();
   await initResponders();
 }
