ea5642af93bf204a49cb8ab7aa3333d67ab06b97
jodywarren <jodytuuta@msn.com>
2026-03-11 16:54:55 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,43 @@
 
+const CACHE_NAME = "conn-sheet-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./service-worker.js",
+  "./connewarre-logo.png",
+  "./js/state.js",
+  "./js/helpers.js",
+  "./js/render.js",
+  "./js/incident.js",
+  "./js/responders.js",
+  "./js/ocr.js",
+  "./js/report.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
