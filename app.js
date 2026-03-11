8f5288429c8b74e3c2620f3b1b45102f88b78f33
jodywarren <jodytuuta@msn.com>
2026-03-11 17:04:29 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-
 import { initState } from "./js/state.js";
 import { bindShellEvents } from "./js/render.js";
+import { bindIncidentInputs } from "./js/incident.js";
 
 document.addEventListener("DOMContentLoaded", initApp);
 
 function initApp() {
+
   initState();
+
   bindShellEvents();
+
+  bindIncidentInputs();
+
 }
