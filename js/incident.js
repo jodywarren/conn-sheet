2ec2d9fcc9856bed139127b07d9d70390daa6f6a
jodywarren <jodytuuta@msn.com>
2026-04-12 21:47:54 +1000

    Update incident.js

--- js/incident.js
+++ js/incident.js
@@ -98,6 +98,7 @@ export function bindIncidentInputs() {
   applyStructureEquipmentToggles();
   applyStructureSectionStates();
 
+  bindMva();
   renderMvaVehicles();
   bindAlarm();
   applyDetailTabCompletionStates();
